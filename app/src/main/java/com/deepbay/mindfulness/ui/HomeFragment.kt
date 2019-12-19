3771925f6ecf1c69c9f507d3440bad2d2b4c7f7c
xuyonghua <1600062751@qq.com>
2019-12-19 18:43:16 +0800

    update homepage

--- app/src/main/java/com/deepbay/mindfulness/ui/HomeFragment.kt
+++ app/src/main/java/com/deepbay/mindfulness/ui/HomeFragment.kt
@@ -1,29 +1,34 @@
 package com.deepbay.mindfulness.ui
 
+import android.content.Context
 import android.os.Bundle
+import android.util.DisplayMetrics
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.WindowManager
 import android.widget.ImageView
+import androidx.cardview.widget.CardView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.deepbay.mindfulness.R
-import com.deepbay.mindfulness.adapter.BannerAdapter
 import com.deepbay.mindfulness.databinding.HomeFragmentBinding
-import com.deepbay.mindfulness.widget.bannerview.BannerLayoutManager
-import com.deepbay.mindfulness.widget.bannerview.BannerPageSnapHelper
 import com.deepbay.mindfulness.widget.bannerview.BannerSetting
+import com.deepbay.mindfulness.widget.bannerview.ScaleBannerLayoutManager
 
 class HomeFragment : Fragment() {
 
     private lateinit var viewModel: HomeViewModel
 
     private val data = listOf(
-        "https://www.wanandroid.com/blogimgs/fa822a30-00fc-4e0d-a51a-d704af48205c.jpeg",
-        "https://www.wanandroid.com/blogimgs/62c1bd68-b5f3-4a3c-a649-7ca8c7dfabe6.png",
-        "https://www.wanandroid.com/blogimgs/90c6cc12-742e-4c9f-b318-b912f163b8d0.png"
+        "https://res-mindfullness-vigour-wechat.oss-cn-shenzhen.aliyuncs.com/images/album/creativity%403x.png",
+        "https://res-mindfullness-vigour-wechat.oss-cn-shenzhen.aliyuncs.com/images/album/stress%403x.png",
+        "https://res-mindfullness-vigour-wechat.oss-cn-shenzhen.aliyuncs.com/images/album/anxiety%403x.png",
+        "https://res-mindfullness-vigour-wechat.oss-cn-shenzhen.aliyuncs.com/images/album/creativity%403x.png",
+        "https://res-mindfullness-vigour-wechat.oss-cn-shenzhen.aliyuncs.com/images/album/focused%403x.png",
+        "https://res-mindfullness-vigour-wechat.oss-cn-shenzhen.aliyuncs.com/images/album/sleep%403x.png"
     )
 
     override fun onCreateView(
@@ -34,17 +39,16 @@ class HomeFragment : Fragment() {
         viewModel = ViewModelProviders.of(this).get(HomeViewModel::class.java)
         binding.viewModel = viewModel
         binding.lifecycleOwner = this
-        binding.banner.layoutManager = BannerLayoutManager()
-        binding.banner.snapHelper = BannerPageSnapHelper()
+//        binding.banner.layoutManager = BannerLayoutManager()
 
-        val adapter = BannerAdapter()
-        adapter.data = data
-        binding.banner.setUp(BannerSetting().apply {
-            slideTimeGap = 3000 // 自动滑动的时间间隔
-            autoSlideSpeed = 1000 // 自动滑动一次的速度
-            loop = true // 是否循环滑动
-            canAutoSlide = true // 是否自动滑动
-        }, adapter)
+//        val adapter = BannerAdapter()
+//        adapter.data = data
+//        binding.banner.setUp(BannerSetting().apply {
+//            slideTimeGap = 3000 // 自动滑动的时间间隔
+//            autoSlideSpeed = 1000 // 自动滑动一次的速度
+//            loop = true // 是否循环滑动
+//            canAutoSlide = true // 是否自动滑动
+//        }, adapter)
 
 //        binding.banner.setUp(BannerSetting().apply {
 //            slideTimeGap = 3000 // 自动滑动的时间间隔
@@ -63,11 +67,43 @@ class HomeFragment : Fragment() {
 //                Glide.with(activity!!).load(data[position]).into(holder.image)
 //            }
 //        })
+        binding.banner.layoutManager = ScaleBannerLayoutManager()
+        binding.banner.setUp(BannerSetting().apply {
+            slideTimeGap = 3000
+            autoSlideSpeed = 1000
+            loop = true
+            canAutoSlide = true
+        }, Adapter())
+
+
 
         return binding.root
     }
 
-    inner class ImageViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    fun getDisplayMetrics(context: Context): DisplayMetrics {
+        val windowManager = context.getSystemService(Context.WINDOW_SERVICE) as WindowManager
+        val metrics = DisplayMetrics()
+        windowManager.defaultDisplay.getMetrics(metrics)
+        return metrics
+    }
+
+    inner class Adapter : RecyclerView.Adapter<ViewHolder>() {
+
+        override fun onCreateViewHolder(group: ViewGroup, position: Int): ViewHolder {
+            val item = LayoutInflater.from(activity).inflate(R.layout.view_banner_item_view, group, false)
+            return ViewHolder(item)
+        }
+
+        override fun getItemCount() = data.size
+
+        override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+            holder.cardItem.layoutParams?.width = (getDisplayMetrics(activity!!).widthPixels.times(0.85f)).toInt()
+            Glide.with(activity!!).load(data[position]).into(holder.image)
+        }
+    }
+
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val image: ImageView = itemView.findViewById(R.id.banner_image)
+        val cardItem: CardView = itemView.findViewById(R.id.card_item)
     }
 }
